f325bf71670cbc6016fd1bb49ca7cb39b282dc55
Miguel L. Turbón <miguel.lorenzo@dacoruna.gal>
2020-05-07 20:56:41 +0200

    Ejemplo de zonas y peticiones ajax de Tapestry

--- modulos/microstack-uno/src/main/java/uno/pages/IndexUno.java
+++ modulos/microstack-uno/src/main/java/uno/pages/IndexUno.java
@@ -1,5 +1,37 @@
 package uno.pages;
 
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.Renderable;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
+
 public class IndexUno {
+    @Inject
+    AjaxResponseRenderer ajaxResponseRenderer;
+    @Inject
+    Block blkSaludo;
+
+    @OnEvent("saludar-es")
+    void doSaludarES() {
+        ajaxResponseRenderer.addRender("znSaludo", blkSaludo);
+    }
+
+    @OnEvent("saludar-gl")
+    void doSaludarGL() {
+        ajaxResponseRenderer.addRender(
+            "znSaludo",
+            (Renderable) writer -> {
+                writer.element("div", "class", "alert alert-info");
+                writer.write("Bos días");
+                writer.end();
+            }
+        );
+    }
+
+    @OnEvent("limpiar")
+    void doLimpiar() {
+        ajaxResponseRenderer.addRender("znSaludo", (Renderable) writer -> {});
+    }
 
 }
